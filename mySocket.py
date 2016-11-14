4717ea2d8376ca1157741bbd5f45931c84fa8175
Chih Liang Wei <bu00312@gmail.com>
2016-11-14 14:00:36 +0800

    Edit URL

--- mySocket.py
+++ mySocket.py
@@ -1,8 +1,8 @@
 import socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect( ('python-data.dr-chuck.net', 80) )
+mysock.connect( ('data.pr4e.org', 80) )
 
-mysock.send('GET http://python-data.dr-chuck.net/regex_sum_42.txt HTTP/1.0\n\n')
+mysock.send('GET http://data.pr4e.org/intro-short.txt HTTP/1.0\n\n')
 while True:
     data = mysock.recv(512)
     if ( len(data) < 1 ) :
